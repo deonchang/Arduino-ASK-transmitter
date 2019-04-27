f931fd5acca07531766b2927fa67152f8078431d
Deon Chang <43723564+deonchang@users.noreply.github.com>
2019-04-27 12:42:38 +1000

    Moved everything out of the main loop and into functions.

--- ask_transmitter/ask_transmitter.ino
+++ ask_transmitter/ask_transmitter.ino
@@ -1,34 +1,61 @@
-const int TX_PIN = 2;          // transmitter pin
-const int BITS = 220;          // number of sequence
-const int BIT_DURATION = 370;  // microseconds
+const int TX_PIN = 13;          // transmitter pin
+const int TX_VCC = 12;          // transmitter VCC pin
+const int PREAMB = 24;          // preamble length
+const int BITS = 197;           // length of data
+const int BIT_DURATION = 370;   // microseconds
+const int PREAMB_DELAY = 3300;  // microseconds
+const int REPEAT_DELAY = 1550;  // microseconds
 
-int bits[BITS] = {};
+int preamble[PREAMB] = {};
+int data[BITS] = {};
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(TX_PIN, OUTPUT); 
+  pinMode(TX_PIN, OUTPUT);
+  pinMode(TX_VCC, OUTPUT);
   digitalWrite(TX_PIN, LOW);
+  digitalWrite(TX_VCC, HIGH);
   digitalWrite(LED_BUILTIN, LOW);
-  Serial.begin(9600);
 }
 
-void loop() {
-  Serial.println("Transmitting...");
-  for (int bit = 0; bit < BITS; bit++) {
+void transmitPreamble() {
+  for (int bit = 0; bit < PREAMB; bit++) {
     // Turn the transmitter on for one bit duration if the bit is a 1
-    if (bits[bit] == 1) {
+    if (preamble[bit] == 1) {
       digitalWrite(TX_PIN, HIGH);
       digitalWrite(LED_BUILTIN, HIGH);
-      delayMicroseconds(BIT_DURATION);
-      digitalWrite(TX_PIN, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
-    } 
+    }
     else {
       // Bit is zero and therefore don't transmit anything for one bit duration
       digitalWrite(TX_PIN, LOW);
       digitalWrite(LED_BUILTIN, LOW);
-      delayMicroseconds(BIT_DURATION);
     }
+    // Transmit for one bit duration
+    delayMicroseconds(BIT_DURATION);
   }
-  Serial.println("Transmission completed!");
+  // Turn off transmitter when we're finished
+  digitalWrite(TX_PIN, LOW);
+}
+
+void transmitSeq() {
+  for (int bit = 0; bit < BITS; bit++) {
+    if (data[bit] == 1) {
+      digitalWrite(TX_PIN, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
+    } 
+    else {
+      digitalWrite(TX_PIN, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    delayMicroseconds(BIT_DURATION);
+  }
+  digitalWrite(TX_PIN, LOW);
+}
+
+
+void loop() {
+  transmitPreamble();
+  delayMicroseconds(PREAMB_DELAY);
+  transmitSeq();
+  delayMicroseconds(REPEAT_DELAY);
 }
